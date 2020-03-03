f7cc888de1f6e47af25879f9cc9ceb6d7b64db32
Emmie Davis <emmiedavis314@gmail.com>
2020-03-03 00:23:39 -0600

    Updates on quiz css

--- pages/quiz.js
+++ pages/quiz.js
@@ -4,10 +4,13 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import React from 'react';
 import { MenuItem } from "@material-ui/core";
+//import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
 
 // using material-ui now https://material-ui.com/components/selects/
 // dynamic forms for later https://itnext.io/building-a-dynamic-controlled-form-in-react-together-794a44ee552c
 // react forms doc https://reactjs.org/docs/forms.html
+// styling material ui for later: https://stackoverflow.com/questions/48319372/changing-font-family-of-all-material-uiversion-1-components
 
 const indexStyle = {
     "border-top": "20px solid maroon",
@@ -16,7 +19,8 @@ const indexStyle = {
 
 class Quiz extends React.Component {
     state = {
-        Major: "None"
+        Major: "None",
+        Minor: "None"
     };
 
     handleChange = event => {
@@ -26,10 +30,25 @@ class Quiz extends React.Component {
     render() {
         return (
             <div style={indexStyle}>
+                <style jsx>{`
+                    @import url('https://fonts.googleapis.com/css?family=Muli|Roboto&display=swap');
+                    h1 {
+                        font-family: 'Muli';
+                        font-size: 48px;
+                    }
+                    p {
+                        font-family: 'Roboto';
+                        font-size: 24px;
+                        max-width: 700px;
+                    }
+                    div {
+                        padding: 80px 100px;
+                    }
+                `}</style>
                 <SideNavLayout />
                 <h1>Welcome to AggieOrgs, NAME.</h1>
-                <p>Descriptive text.</p>
-                <FormControl>
+                <p>Please fill out this preliminary form to help us better suggest relevant organizations to you.</p>
+                <FormControl style={{minWidth: 400}}>
                     <InputLabel id="majors">Your major:</InputLabel>
                     <Select 
                         labelId="major-1-label"
@@ -41,7 +60,22 @@ class Quiz extends React.Component {
                         <MenuItem value="Computing">Computing</MenuItem>
                     </Select>
                 </FormControl>
-                <button>Add new major</button>
+                <Button style={{"margin-left": 30},{"margin-top": 15}}>Add second major</Button> 
+            
+                <FormControl style={{minWidth: 400}}>
+                    <InputLabel id="minors">Your minor:</InputLabel>
+                    <Select 
+                        labelId="minot-1-label"
+                        id="minor-1"
+                        value={this.state.Minor}
+                        onChange={this.handleChange}
+                    >
+                        <MenuItem value="Computer Science">Computer Science</MenuItem>
+                        <MenuItem value="Computing">Computing</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button style={{"margin-left": 30},{"margin-top": 15}}>Add second minor</Button> 
+
             </div>
         );
     }

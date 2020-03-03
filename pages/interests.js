daa008b115e80ca0c9e672225933f315e97e5c64
alex-d-n-pham <fragcakes@gmail.com>
2020-03-03 14:48:17 -0600

    recommender with fetch for temp user data

--- pages/interests.js
+++ pages/interests.js
@@ -1,13 +1,74 @@
 import React from 'react';
 import SideNavLayout from "../components/SideNavLayout";
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Button from '@material-ui/core/Button';
+import Link from 'next/link';
+import fetch from 'isomorphic-fetch'
+
+const indexStyle = {
+    "border-top": "20px solid maroon",
+    width: "100%"
+};
 
 class Interests extends React.Component {
+    state = {
+        academic: false,
+        religious: false,
+        fineArts: false
+    }
+
+    handleChange = name => event => {
+        this.setState({[name]: event.target.checked }, () =>
+            {console.log(this.state);}
+        );
+    }
+
+    handleSubmit = event => {
+        
+    }
+
     render() {
         return (
-            <div>
+            <div style={indexStyle}>
                 <SideNavLayout />
                 <h1>Welcome to AggieOrgs, NAME.</h1>
                 <p>Descriptive text.</p>
+
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Choose categories of interest.</FormLabel>
+                        <FormGroup>
+                            <FormControlLabel
+                                control={<Checkbox checked={this.state.academic} onChange={this.handleChange('academic')} value="academic" />}
+                                label="Academic"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox checked={this.state.religious} onChange={this.handleChange('religious')} value="religious" />}
+                                label="Religious"
+                            />
+                            <FormControlLabel
+                                control={
+                                <Checkbox checked={this.state.fineArts} onChange={this.handleChange('fineArts')} value="fineArts" />
+                                }
+                                label="Fine Arts"
+                            />
+                        </FormGroup>
+                    <FormHelperText>Click button when done</FormHelperText>
+                </FormControl>
+
+                <Button 
+                    onClick={this.handleSubmit}
+                    color="secondary"
+                    variant="contained"
+                >
+                    <Link href="/recommender">
+                        Recommender ->
+                    </Link>
+                </Button>
             </div>
         ); 
     } 
---
+++ pages/recommender.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import SideNavLayout from "../components/SideNavLayout";
+import fetch from 'isomorphic-fetch'
+
+// https://www.robinwieruch.de/react-fetching-data to show how to make the components update in time
+
+const indexStyle = {
+    "border-top": "20px solid maroon",
+    width: "100%"
+};
+
+class Recommender extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: {}
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://aggieorgs-backend-270016.appspot.com/api/v1/user/27ea0040-5d82-11ea-bf1c-8b58b3001807')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            this.setState({data: data}, () => 
+                {console.log(this.state);}
+            );
+        });
+    }
+
+    handleChange = name => event => {
+        this.setState({[name]: event.target.checked }, () =>
+            {console.log(this.state);}
+        );
+    }
+
+    render() {
+        return (
+            <div style={indexStyle}>
+                <SideNavLayout />
+                <h1>Welcome to AggieOrgs, NAME.</h1>
+                <p>Descriptive text.</p>
+                <p>{this.state.data.userId}</p>
+                <p>{this.state.data.userFirstName}</p>
+                <p>{this.state.data.userLastName}</p>
+            </div>
+        );
+    }
+}
+
+export default Recommender;
